5b994dc5c5610c08624c29ab85bcd658f1172ef0
Keroosha <mr.dead.toast@gmail.com>
2018-12-16 01:51:12 +0300

    Asp.net core project init, some views ported to razor MVC

---
+++ ProfBusWeb/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProfBusWeb.Models;
+
+namespace ProfBusWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactUs(string Name, string Phone, string Address, string Text)
+        {
+            ViewData["Name"] = Name;
+            return View();
+        }
+    }
+}
---
+++ ProfBusWeb/Models/ContactUsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProfBusWeb.Models
+{
+    public class ContactUsModel
+    {
+        [Required]
+        public string Name;
+
+        [Required]
+        public string Address;
+
+        [Required]
+        public string Text;
+
+        [Required]
+        public string Phone;
+    }
+}
